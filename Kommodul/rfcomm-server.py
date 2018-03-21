2592d81c78ecee862fdd520a3d1fd0d9f086e59b
Martin Allander <aldetan18@gmail.com>
2018-03-21 14:41:03 +0100

    Working bluetooth connection server

--- Kommodul/rfcomm-server.py
+++ Kommodul/rfcomm-server.py
@@ -1,16 +1,23 @@
 import bluetooth
+import time
 
 server_sock=bluetooth.BluetoothSocket( bluetooth.RFCOMM )
 
-port = 1
+port = 6
+#server_sock.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
 server_sock.bind(("",port))
 server_sock.listen(1)
 
 client_sock,address = server_sock.accept()
 print "Accepted connection from ",address
 
-data = client_sock.recv(1024)
-print "received [%s]" % data
+tic = time.clock()
 
+data = client_sock.recv(1024)
+#print "received [%s]" % data
+
+toc = time.clock()
+
+print(toc-tic)
 client_sock.close()
 server_sock.close()
