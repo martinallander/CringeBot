dfef9f5b7cdac27567ddf023b744ee15dc5c4fe7
martinallander <martin.allander@gmail.com>
2018-03-21 14:39:24 +0100

    Working bluetooth connection client

--- Kommodul/rfcomm-client.py
+++ Kommodul/rfcomm-client.py
@@ -1,12 +1,21 @@
 import bluetooth
+import time
+
+
 
 bd_addr = "98:5F:D3:35:FC:EA"
 
-port = 1
+port = 6
 
 sock=bluetooth.BluetoothSocket( bluetooth.RFCOMM )
 sock.connect((bd_addr, port))
 
-sock.send("hello!!")
+tic = time.clock()
+i = 1
+while i < 10:
+	sock.send("xD\n")
+	i = i + 1
 
+toc = time.clock()
+print(toc-tic)
 sock.close()
