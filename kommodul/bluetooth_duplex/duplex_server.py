c7daa1bcd7c8f6427c8fd678c1adcc02becf86c9
Martin Allander <aldetan18@gmail.com>
2018-03-22 11:08:47 +0100

    New server funcitonality

--- kommodul/bluetooth_duplex/duplex_server.py
+++ kommodul/bluetooth_duplex/duplex_server.py
@@ -1,5 +1,6 @@
 import bluetooth
 import time
+import pickle
 
 server_sock=bluetooth.BluetoothSocket( bluetooth.RFCOMM )
 
@@ -12,13 +13,27 @@ server_sock.listen(1)
 client_sock,address = server_sock.accept()
 print "Accepted connection from ",address
 
+upkl_list = list()
+
 tic = time.clock()
+i = 0
+while True:
+    data = client_sock.recv(1024)
+    upkl = pickle.loads(data)
+    upkl_list.append(upkl)
+    if upkl.stop:
+        break
+    if i == 20:
+        break
+    i += 1
 
-data = client_sock.recv(1024)
-#print "received [%s]" % data
-
-toc = time.clock()
-
-print(toc-tic)
 client_sock.close()
 server_sock.close()
+
+for item in upkl_list:
+    print(item)
+#print "received [%s]" % data
+#toc = time.clock()
+#print(toc-tic)
+
+
