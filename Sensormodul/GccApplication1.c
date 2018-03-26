09b893b969e604d01e1b11559e2f5faf33c27240
MatKinPro <matki347@student.liu.se>
2018-03-26 16:43:09 +0200

    SPI slave sensor

--- Sensormodul/GccApplication1.c
+++ Sensormodul/GccApplication1.c
@@ -17,6 +17,10 @@
 #define IR_OK 0xA2
 #define MIC_OK 0xE6
 
+#define ACCSIZE 12
+#define TOFSIZE 2
+#define IRSIZE 64
+
 
 #include <avr/io.h>
 #include <avr/interrupt.h>
@@ -50,30 +54,61 @@ void led_blink (uint16_t i)
 
 void acc_gyro_data(void)
 {
+	unsigned char error; 
 	spi_tranceiver(ACC_OK);
-	spi_tranceiver();
+	error = spi_tranceiver(acc_send_data[0]); //ADRESS FÖR ACCELEROMETERDATA X MSB
+	if (error == ERROR)
+	{
+		return;
+	}
+	for (int i = 1; i << ACCSIZE; i++)
+	{
+		spi_tranceiver(acc_send_data[i]);
+	}
 	spi_tranceiver(ACC_OK);
 	return;
 }
 
 void tof_data(void)
 {
+	unsigned char error; 
 	spi_tranceiver(TOF_OK);
-	spi_tranceiver();
+	error = spi_tranceiver(tof_send_data[0]); //ADRESS FÖR ACCELEROMETERDATA X MSB
+	if (error == ERROR)
+	{
+		return;
+	}
+	for (int i = 1; i << TOFSIZE; i++)
+	{
+		spi_tranceiver(tof_send_data[i]);
+	}
 	spi_tranceiver(TOF_OK);
 	return;
 }
 
 void ir_data(void)
 {
+	unsigned char error; 
 	spi_tranceiver(IR_OK);
-	spi_tranceiver();
+	error = spi_tranceiver(ir_send_data[0]); //ADRESS FÖR ACCELEROMETERDATA X MSB
+	if (error == ERROR)
+	{
+		return;
+	}
+	for (int i = 1; i << IRSIZE; i++)
+	{
+		spi_tranceiver(ir_send_data[i]);
+	}
 	spi_tranceiver(IR_OK);
 	return;
 }
 
 int main(void)
 {
+	unsigned char[ACCSIZE] acc_sendable_data = 0;
+	unsigned char[TOFSIZE] tof_sendable_data = 0;
+	unsigned char[IRSIZE] ir_sendable_data = 0;
+
 	spi_init_slave();                             //Initialize slave SPI
 	DDRB |= 0x01;								  //PB0 as Output
 	unsigned char data = 0;
