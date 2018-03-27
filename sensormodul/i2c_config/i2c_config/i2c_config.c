81d00e79f8eaf9c7ad4519c1bc16b22d5ba1b29f
perliljestrom <jrperka@gmail.com>
2018-03-27 17:41:41 +0200

    TYP FUCKING KLARgit add i2c_config.c!

--- sensormodul/i2c_config/i2c_config/i2c_config.c
+++ sensormodul/i2c_config/i2c_config/i2c_config.c
@@ -10,13 +10,21 @@
 #endif
 
 #include <avr/io.h>
-#include <avr/delay.h>
+#include <util/delay.h>
 #include <avr/interrupt.h>
 #include <util/twi.h>
 
 #define F_SCL 100000UL
 #define Prescaler 1
 #define TWBR_value ((((F_CPU/F_SCL)/Prescaler)-16)/2)
+
+#define ERROR 1
+#define SUCCESS 0
+
+#define I2C_WRITE	0
+#define I2C_READ	1
+
+#define accel_addr 0x32
 
 //volatile unsigned char i2c_addr;
 
@@ -28,18 +36,21 @@ void i2c_init(void)
 	TWCR = (1 << TWEN);
 }
 
-uint8_t i2c_start(uint8_t sad_addr) //Ta in slavens address + skriv/läs. SAD + W/R SKA VARA W-bit!
+uint8_t i2c_start(uint8_t addr) //Ta in slavens address + skriv/läs. SAD + W/R SKA VARA W-bit!
 {
+	//reset I2C control register
+	//TWCR = 0;
+	
 	TWCR = (1 << TWINT)|(1 << TWSTA)|(1 << TWEN);
 	
 	while(!(TWCR & (1 << TWINT)));
 	
 	if((TWSR & 0xF8) != TW_START)
 	{
-		return 1;
+		return ERROR;
 	}
 	//laddar TWDR med slavadressen
-	TWDR = sad_addr;
+	TWDR = addr;
 	//starta transmissionen av adressen
 	TWCR = (1 << TWINT)|(1 << TWEN);
 	//Vänta för att transmissionen ska bli klar
@@ -49,10 +60,15 @@ uint8_t i2c_start(uint8_t sad_addr) //Ta in slavens address + skriv/läs. SAD + W
 	//kolla om en acknowledge fåtts
 	if((twst != TW_MT_SLA_ACK) && (twst != TW_MR_SLA_ACK))
 	{
-		return 1;
+		return ERROR;
 	} 
 	
-	return 0;
+	return SUCCESS;
+}
+
+uint8_t i2c_repeated_start(uint8_t addr)
+{
+	return i2c_start(addr);
 }
 
 void i2c_stop(void)
@@ -69,10 +85,10 @@ uint8_t i2c_write(uint8_t data)
 	
 	if((TWSR & 0xF8) != TW_MT_DATA_ACK)
 	{
-		return 1;
+		return ERROR;
 	}
 	
-	return 0;
+	return SUCCESS;
 }
 
 uint8_t i2c_ack_read(void)
@@ -89,69 +105,60 @@ uint8_t i2c_nack_read(void)
 	return TWDR;
 }
 
-//kan vara fel att använda dessa. De utkommenterade funk. längst ned kan vara
-//lämpligare. problemet som uppstår är "data-pekaren". Antingen gör om till vanlig
-//int. eller använda utkommenterade funktioner typ.
-uint8_t i2c_write_register(uint8_t device_addr, uint8_t register_addr, uint8_t* data, uint16_t length)
+
+void accel_i2c_write_reg(uint8_t reg_addr, uint8_t data)
 {
-	if (i2c_start(device_addr | 0x00))
-	{
-		return 1;
-	}
-	
-	i2c_write(register_addr);
-
-	for (uint16_t i = 0; i < length; i++)
-	{
-		if (i2c_write(data[i])) 
-		{
-			return 1;
-		}
-	}
-	
+	i2c_start(accel_addr + I2C_WRITE);
+	i2c_write(reg_addr);
+	i2c_write(data);
 	i2c_stop();
+}
 
-	return 0;
+//Data är där data sparas
+void accel_i2c_read_reg(uint8_t reg_addr, uint8_t data)
+{
+	i2c_start(accel_addr + I2C_WRITE);
+	i2c_write(reg_addr);
+	i2c_repeated_start(accel_addr + I2C_READ);
+	//osäker på om man ska ha och hur data = i2c_nack_read() fungerar...
+	data = i2c_nack_read();
+	i2c_stop();	
 }
 
 /*
-vad ska egentligen "data"-parametern vara? där kommer den lästa datan att hamna.
-men om man tar TWDR som jag tror, så skriver man ju TWDR till TWDR. Man kanske ska
-skriva till nån port vid test för att senare skriva till SPI kanalen för att skicka
-vidare till Raspberry PI:n?
+OBS! Måste skapa en array-variabel för att spara undan datan. för att sedan tillkalla
+funktionen med. Kom ihåg längd på arrayen. Finns dynamisk variant. MALLOC google är är
+din vän.
 */
-uint8_t i2c_read_register(uint8_t device_addr, uint8_t register_addr, uint8_t* data, uint16_t length)
+void accel_i2c_multiple_read_reg(uint8_t reg_addr, uint8_t* data, uint16_t length)
 {
-	if (i2c_start(device_addr))
+	i2c_start(accel_addr + I2C_WRITE);
+	i2c_write(reg_addr);
+	i2c_repeated_start(accel_addr + I2C_READ);
+	
+	for (uint16_t i = 0; i < (length -1); i++)
 	{
-		return 1;
-	}
 
-	i2c_write(register_addr);
-
-	if (i2c_start(device_addr | 0x01)) 
-	{
-		return 1;
-	}
-
-	for (uint16_t i = 0; i < (length-1); i++)
-	{
 		data[i] = i2c_ack_read();
 	}
+	data[(length - 1)] = i2c_nack_read();
 	
-	data[(length-1)] = i2c_nack_read();
-
 	i2c_stop();
-
-	return 0;
 }
 
-uint8_t i2c_get_status(void)
-{
-	uint8_t status;
-	status = (TWSR & 0xF8);
-	return status;
-}
+/*
+hur man "Castar" till ny datatyp ex: av Oscar!
+uint8_t sparad_data_16_B
+(float) sparad_data_16_B
+*/
+
+
+//uint8_t i2c_get_status(void)
+//{
+	//uint8_t status;
+	//status = (TWSR & 0xF8);
+	//return status;
+//}
 
 void led_blink (uint16_t i)
 {
@@ -165,36 +172,10 @@ void led_blink (uint16_t i)
 	}
 }
 
+
 int main(void)
 {
-	#define accel_addr_write	0x32
-	#define accel_addr_read		0x33
-	
-	#define test_register_addr 0x20
-	
-	DDRB = (1 << DDB0); //output
-	
-	//initiering av I2C
-	i2c_init();
-	
-	while (1)
-	{
-		i2c_write_register(accel_addr_write, test_register_addr, 0x47, 0x0008);
-		
-		i2c_read_register(accel_addr_read, test_register_addr, TWDR, 0x0008);
-		
-		if (TWDR == 0x47)
-		{
-			led_blink(10);
-		} 
-		else
-		{
-			led_blink(5);
-		}
-		//Behövs det en return här??
-		return 0;
-	}
-	//return 0;
+	return SUCCESS;
 }
 
 
@@ -235,5 +216,62 @@ int main(void)
 	//
 	//i2c_stop();
 	//
+	//return 0;
+//}
+
+////kan vara fel att använda dessa. De utkommenterade funk. längst ned kan vara
+////lämpligare. problemet som uppstår är "data-pekaren". Antingen gör om till vanlig
+////int. eller använda utkommenterade funktioner typ.
+//uint8_t i2c_write_register(uint8_t device_addr, uint8_t register_addr, uint8_t* data, uint16_t length)
+//{
+	//if (i2c_start(device_addr | 0x00))
+	//{
+		//return 1;
+	//}
+	//
+	//i2c_write(register_addr);
+//
+	//for (uint16_t i = 0; i < length; i++)
+	//{
+		//if (i2c_write(data[i])) 
+		//{
+			//return 1;
+		//}
+	//}
+	//
+	//i2c_stop();
+//
+	//return 0;
+//}
+//
+///*
+//vad ska egentligen "data"-parametern vara? där kommer den lästa datan att hamna.
+//men om man tar TWDR som jag tror, så skriver man ju TWDR till TWDR. Man kanske ska
+//skriva till nån port vid test för att senare skriva till SPI kanalen för att skicka
+//vidare till Raspberry PI:n?
+//*/
+//uint8_t i2c_read_register(uint8_t device_addr, uint8_t register_addr, uint8_t* data, uint16_t length)
+//{
+	//if (i2c_start(device_addr))
+	//{
+		//return 1;
+	//}
+//
+	//i2c_write(register_addr);
+//
+	//if (i2c_start(device_addr | 0x01)) 
+	//{
+		//return 1;
+	//}
+//
+	//for (uint16_t i = 0; i < (length-1); i++)
+	//{
+		//data[i] = i2c_ack_read();
+	//}
+	//
+	//data[(length-1)] = i2c_nack_read();
+//
+	//i2c_stop();
+//
 	//return 0;
 //}
