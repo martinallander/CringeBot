ea9c1d33d736b826652679caed881330b5385b9f
perliljestrom <jrperka@gmail.com>
2018-03-27 15:37:09 +0200

    Typ klar med testet. tyvärr gick saker snett. men men

--- sensormodul/i2c_config/i2c_config/i2c_config.c
+++ sensormodul/i2c_config/i2c_config/i2c_config.c
@@ -28,7 +28,7 @@ void i2c_init(void)
 	TWCR = (1 << TWEN);
 }
 
-void i2c_start(uint8_t sad_addr) //Ta in slavens address + skriv/läs. SAD + W/R SKA VARA W-bit!
+uint8_t i2c_start(uint8_t sad_addr) //Ta in slavens address + skriv/läs. SAD + W/R SKA VARA W-bit!
 {
 	TWCR = (1 << TWINT)|(1 << TWSTA)|(1 << TWEN);
 	
@@ -46,6 +46,7 @@ void i2c_start(uint8_t sad_addr) //Ta in slavens address + skriv/läs. SAD + W/R 
 	while(!(TWCR & (1 << TWINT)));
 	
 	uint8_t twst = TW_STATUS & 0xF8;
+	//kolla om en acknowledge fåtts
 	if((twst != TW_MT_SLA_ACK) && (twst != TW_MR_SLA_ACK))
 	{
 		return 1;
@@ -59,7 +60,7 @@ void i2c_stop(void)
 	TWCR = (1 << TWINT)|(1 << TWSTO)|(1 << TWEN);
 }
 
-void i2c_write(uint8_t data)
+uint8_t i2c_write(uint8_t data)
 {
 	TWDR = data;
 	TWCR = (1 << TWINT)|(1 << TWEN);
@@ -88,44 +89,9 @@ uint8_t i2c_nack_read(void)
 	return TWDR;
 }
 
-uint8_t i2c_transmit(uint8_t addr, uint8_t* data, uint16_t length)
-{
-	//Osäker på om en startsignal ska skickas innan skrivning. Kan fungera som kontroll
-	// av lyckad överföring eller liknande???
-	if (i2c_start(addr | I2C_WRITE))
-	{
-		return 1;
-	} 
-	
-	for (uint16_t i = 0; i < length; i++)
-	{
-		if (i2c_write(data[i])) return 1;
-	}
-	
-	i2c_stop();
-	
-	return 0;
-}
-
-uint8_t i2c_receive(uint8_t addr, uint8_t* data, uint16_t length)
-{
-	if (i2c_start(addr | I2C_READ))
-	{			
-		return 1;
-	}
-	
-	for (uint16_t i = 0; i < (length-1); i++)
-	{
-		data[i] = i2c_ack_read();
-	}
-	
-	data[(length-1)] = i2c_nack_read();
-	
-	i2c_stop();
-	
-	return 0;
-}
-
+//kan vara fel att använda dessa. De utkommenterade funk. längst ned kan vara
+//lämpligare. problemet som uppstår är "data-pekaren". Antingen gör om till vanlig
+//int. eller använda utkommenterade funktioner typ.
 uint8_t i2c_write_register(uint8_t device_addr, uint8_t register_addr, uint8_t* data, uint16_t length)
 {
 	if (i2c_start(device_addr | 0x00))
@@ -148,6 +114,12 @@ uint8_t i2c_write_register(uint8_t device_addr, uint8_t register_addr, uint8_t* 
 	return 0;
 }
 
+/*
+vad ska egentligen "data"-parametern vara? där kommer den lästa datan att hamna.
+men om man tar TWDR som jag tror, så skriver man ju TWDR till TWDR. Man kanske ska
+skriva till nån port vid test för att senare skriva till SPI kanalen för att skicka
+vidare till Raspberry PI:n?
+*/
 uint8_t i2c_read_register(uint8_t device_addr, uint8_t register_addr, uint8_t* data, uint16_t length)
 {
 	if (i2c_start(device_addr))
@@ -181,7 +153,87 @@ uint8_t i2c_get_status(void)
 	return status;
 }
 
+void led_blink (uint16_t i)
+{
+	//Blink LED "i" number of times
+	for (; i>0; --i)
+	{
+		PORTB |= (1<<0);
+		_delay_ms(100);
+		PORTB = (0<<0);
+		_delay_ms(100);
+	}
+}
+
 int main(void)
 {
-  return 0;
+	#define accel_addr_write	0x32
+	#define accel_addr_read		0x33
+	
+	#define test_register_addr 0x20
+	
+	DDRB = (1 << DDB0); //output
+	
+	//initiering av I2C
+	i2c_init();
+	
+	while (1)
+	{
+		i2c_write_register(accel_addr_write, test_register_addr, 0x47, 0x0008);
+		
+		i2c_read_register(accel_addr_read, test_register_addr, TWDR, 0x0008);
+		
+		if (TWDR == 0x47)
+		{
+			led_blink(10);
+		} 
+		else
+		{
+			led_blink(5);
+		}
+		//Behövs det en return här??
+		return 0;
+	}
+	//return 0;
 }
+
+
+
+
+//uint8_t i2c_transmit(uint8_t addr, uint8_t* data, uint16_t length)
+//{
+	////Osäker på om en startsignal ska skickas innan skrivning. Kan fungera som kontroll
+	//// av lyckad överföring eller liknande???
+	//if (i2c_start(addr | I2C_WRITE))
+	//{
+		//return 1;
+	//}
+	//
+	//for (uint16_t i = 0; i < length; i++)
+	//{
+		//if (i2c_write(data[i])) return 1;
+	//}
+	//
+	//i2c_stop();
+	//
+	//return 0;
+//}
+//
+//uint8_t i2c_receive(uint8_t addr, uint8_t* data, uint16_t length)
+//{
+	//if (i2c_start(addr | I2C_READ))
+	//{
+		//return 1;
+	//}
+	//
+	//for (uint16_t i = 0; i < (length-1); i++)
+	//{
+		//data[i] = i2c_ack_read();
+	//}
+	//
+	//data[(length-1)] = i2c_nack_read();
+	//
+	//i2c_stop();
+	//
+	//return 0;
+//}
