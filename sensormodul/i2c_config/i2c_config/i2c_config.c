900a3ca8cf06a2b0910b89e68d680aee4316c218
perliljestrom <jrperka@gmail.com>
2018-04-11 11:21:22 +0200

    Working Gyro values

--- sensormodul/i2c_config/i2c_config/i2c_config.c
+++ sensormodul/i2c_config/i2c_config/i2c_config.c
@@ -50,12 +50,14 @@
 #define acc_z_l_reg 0x2C
 #define acc_z_h_reg 0x2D
 
+volatile bool i2c_done = 1;
 volatile bool write_to_slave = 1;
 volatile uint8_t register_addr;
 volatile uint8_t trans_data;
 volatile uint8_t rec_data;
 volatile int n_o_writes = 0;
 
+#define i2c_mode
 //volatile unsigned char i2c_addr;
 
 void led_blinker(uint8_t times)
@@ -64,9 +66,9 @@ void led_blinker(uint8_t times)
 	for (uint8_t i = 0; i < times; i++)
 	{
 		PORTB |= (1<<0);
-		_delay_ms(400);
+		_delay_ms(500);
 		PORTB = (0<<0);
-		_delay_ms(400);
+		_delay_ms(500);
 	}
 }
 
@@ -81,11 +83,13 @@ void i2c_init(void)
 void i2c_start(void)
 {
 	TWCR = (1 << TWINT)|(1 << TWSTA)|(0 << TWSTO)|(1 << TWEN)|(1 << TWIE);
+	i2c_done = 0;
 }
 
 void i2c_stop(void)
 {
 	TWCR = (1 << TWINT)|(0 << TWSTA)|(1 << TWSTO)|(1 << TWEN)|(1 << TWIE);
+	i2c_done = 1;
 }
 
 void i2c_send_data(uint8_t data)
@@ -102,7 +106,13 @@ short shift_data(uint8_t high, uint8_t low)
 	return (short) data;
 }
 
-
+/*===========================================================================================
+Interrupt for I2C
+-Can only handle single write and read at the moment
+-repeated start is "hard coded" only for read
+-n_o_writes needs to be replace and probably remade, as for now it become negative
+-Wondering if the i2c_stop should not set TWIE to 1, it should stop there.
+===========================================================================================*/
 
 ISR(TWI_vect)
 {
@@ -117,10 +127,10 @@ ISR(TWI_vect)
 		break;
 		
 	case TW_MT_SLA_ACK: //3
-		i2c_send_data(register_addr); //tans_data = register vi vill läsa från
+		i2c_send_data(register_addr); //load the register we want to handle
 		break;
 	case TW_MT_SLA_NACK: //4
-		led_blinker(15);
+		led_blinker(1);
 		i2c_stop();
 		break;
 	case TW_MT_DATA_ACK: //5
@@ -149,6 +159,12 @@ ISR(TWI_vect)
 		rec_data = TWDR;
 		i2c_stop();
 		break;
+	case TW_MR_DATA_ACK: //8
+		rec_data = TWDR;
+			led_blinker(3);
+		i2c_stop();
+
+		break;
 	}
 }
 
@@ -156,6 +172,7 @@ ISR(TWI_vect)
 
 void i2c_write_reg(uint8_t reg_addr, uint8_t data, int n)
 {
+	while(!i2c_done){};
 	n_o_writes = n;
 	register_addr = reg_addr;
 	trans_data = data;
@@ -165,6 +182,7 @@ void i2c_write_reg(uint8_t reg_addr, uint8_t data, int n)
 
 uint8_t i2c_read_reg(uint8_t reg_addr)
 {
+	while(!i2c_done){};
 	register_addr = reg_addr;
 	write_to_slave = 0;
 	i2c_start();
@@ -174,25 +192,47 @@ uint8_t i2c_read_reg(uint8_t reg_addr)
 
 int main(void)
 {
+	volatile uint8_t out;
+	volatile uint8_t x_l_value;
+	volatile uint8_t x_h_value;
+	volatile uint8_t y_l_value;
+	volatile uint8_t y_h_value;
+	volatile uint8_t z_l_value;
+	volatile uint8_t z_h_value;
 	DDRB = (1 << DDB0);
 	PORTB = (0 << PORTB0);
 	i2c_init();
 	sei();
 	uint8_t set_ctrl_reg_1 = 0b01000111;
+	i2c_write_reg(ctrl_reg_1, set_ctrl_reg_1, 1);
 	while(1)
 	{
-		i2c_write_reg(ctrl_reg_1, set_ctrl_reg_1, 1);
-		_delay_ms(100);
-		i2c_read_reg(ctrl_reg_1);
-		uint8_t out = rec_data;
-		if (out == set_ctrl_reg_1)
-		{
-			led_blinker(5);
-		}
-		else
-		{
-			led_blinker(10);
-		}
+		_delay_ms(10);
+	//	i2c_read_reg(ctrl_reg_1);
+		x_l_value = i2c_read_reg(acc_x_l_reg);
+		//x_l_value = rec_data;
+		//out = rec_data;
+		x_h_value = i2c_read_reg(acc_x_h_reg);
+	//	x_h_value = rec_data;
+		
+		/*
+		i2c_read_reg(acc_y_l_reg);
+		y_l_value = rec_data;
+		i2c_read_reg(acc_y_h_reg);
+		y_h_value = rec_data;
+		i2c_read_reg(acc_z_l_reg);
+		z_l_value = rec_data;
+		i2c_read_reg(acc_z_h_reg);
+		z_h_value = rec_data;
+		*/
+		//if (out == set_ctrl_reg_1)
+		//{
+			//led_blinker(5);
+		//}
+		//else
+		//{
+		//led_blinker(3);
+		//}
 	}
 	return 0;
 }
