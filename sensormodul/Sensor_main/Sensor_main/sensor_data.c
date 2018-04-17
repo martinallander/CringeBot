6a991b484c9ffe301eeb4cff5f39b7032755447c
martinallander <martin.allander@gmail.com>
2018-04-17 16:12:55 +0200

    new name

--- sensormodul/Sensor_main/Sensor_main/sensor_data.c
+++ sensormodul/Sensor_main/Sensor_main/sensor_data.c
@@ -1,30 +1,10 @@
+#include "Sensor_Data.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 //typedef unsigned char byte;
 
-struct Sensor_Data
-{
-	short acc_x;
-	short acc_y;
-	short acc_z;
-	short gyro_x;
-	short gyro_y;
-	short gyro_z;
-	float tof_distance;
-	float ir[64];
-};
-
-#define PACKET_SIZE (sizeof(Sensor_Data))
-
-union SPI_Packet
-{
-	Sensor_Data sd;
-	unsigned char packet[sizeof(Sensor_Data)];
-}; typedef union SPI_Packet SPI_Packet;
-
-/*
 int main()
 {
 	SPI_Packet sp;
@@ -42,4 +22,3 @@ int main()
 	printf("%d\n", sp2.sd.acc_y);
 	printf("%d\n", sp2.sd.acc_z);
 }
-*/
